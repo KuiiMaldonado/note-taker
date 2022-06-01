368c303bf7916f5777a734b45e754e01d6c888a6
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-06-01 13:19:10 -0500

    C:/Program Files/Git/notes POST method saves new notes in the db.json file

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const {raw, json} = require("express");
 
 
 router.get('/notes', (req, res) => {
@@ -13,8 +14,29 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    // res.sendFile(path.join(__dirname, '/public/notes.html'));
-    console.log('POST de notes');
+    if (req.body && req.body.title && req.body.text) {
+        const note = {
+            title: req.body.title,
+            text: req.body.text
+        }
+        fs.readFile(path.join(__dirname,'../../db/db.json'), 'utf-8', (err, data) => {
+            if (err) {
+                res.send(err);
+            }
+            let fileData = JSON.parse(data);
+            fileData.push(note);
+            console.log(fileData);
+            fs.writeFile(path.join(__dirname,'../../db/db.json'), JSON.stringify(fileData), (err) => {
+                if (err) {
+                    res.send(err);
+                }
+                res.status(200).send()
+            });
+        });
+    }
+    else {
+        res.status(400).json('Request body must contain a title and text');
+    }
 });
 
 router.delete('/notes/:id', (req, res) => {
